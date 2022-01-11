dff21fc9dd8734b453baf4f32fd9e088e5bed0b4
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-11 18:13:58 +0100

    Adding css, html and js files

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let myName = 'Bob';
+document.write(myName);
+
+document.write("<br>");
+myName = 'John Doe';
+document.write(myName);
+
+document.write("<br>");
+let simpleAddition = 169 + 10345;
+document.write(simpleAddition);
+
+document.write("<br>");
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
