7781f2b7ab12564f65f70f8db61726bf8957e153
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-11 18:36:13 +0100

    change content of js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
-let myName = 'Bob';
-document.write(myName);
+alert('Hello world');
 
-document.write("<br>");
-myName = 'John Doe';
-document.write(myName);
-
-document.write("<br>");
-let simpleAddition = 169 + 10345;
-document.write(simpleAddition);
-
-document.write("<br>");
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let favoriteFood = 'Raclette';
+document.write(favoriteFood);
