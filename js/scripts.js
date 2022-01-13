72e3e9fd1859da241071e9bee888d249090cb41a
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-13 13:49:22 +0100

    Update scripts.js with pokemonList array
    Add pokemonList array with 5 different pokemon objects, containing the keys "name", "height", "type" and "abilities"

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = 'Raclette';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Pikachu" , height: 0.4, type: "electric", abilities: ["Static", "Lightningrod"]},
+  {name: "Poliwag" , height: 0.6, type: "water", abilities: ["Damp","Water-absorb","Swift-swim"]},
+  {name: "Abra", height: 0.9, type: "psychic", abilities: ["Synchronize, Inner-focus, Magic-guard"]},
+  {name: "Magnemite", height: 0.3, type: ["electric, steel"], abilities: ["Sturdy", "Magnet-pull", "Analytic"] },
+  {name: "Sharpedo" , height: 1.8, type: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
+]
