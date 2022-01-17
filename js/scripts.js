4d7d8c04e09a2428212fa134d33537aca32e9981
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-17 16:55:58 +0100

    Update name of "type" object  & add a loop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 let pokemonList = [
-  {name: "Pikachu" , height: 0.4, type: "electric", abilities: ["Static", "Lightningrod"]},
-  {name: "Poliwag" , height: 0.6, type: "water", abilities: ["Damp","Water-absorb","Swift-swim"]},
-  {name: "Abra", height: 0.9, type: "psychic", abilities: ["Synchronize, Inner-focus, Magic-guard"]},
-  {name: "Magnemite", height: 0.3, type: ["electric, steel"], abilities: ["Sturdy", "Magnet-pull", "Analytic"] },
-  {name: "Sharpedo" , height: 1.8, type: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
+  {name: "Pikachu" , height: 0.4, types: "electric", abilities: ["Static", "Lightningrod"]},
+  {name: "Poliwag" , height: 0.6, types: "water", abilities: ["Damp","Water-absorb","Swift-swim"]},
+  {name: "Abra", height: 0.9, types: "psychic", abilities: ["Synchronize, Inner-focus, Magic-guard"]},
+  {name: "Magnemite", height: 0.3, types: ["electric, steel"], abilities: ["Sturdy", "Magnet-pull", "Analytic"] },
+  {name: "Sharpedo" , height: 1.8, types: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
 ]
+
+let text= "";
+let i = 0;
+for (;pokemonList[i];) {
+  text= text  + " " +  pokemonList.name[i] + "height: " + pokemonList.height[i];
+  i++
+}
+document.write (text);
+}
