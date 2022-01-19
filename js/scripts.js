7dfe104e0f7bf95df0d941162c2fd38578895725
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-19 11:04:21 +0100

    Change order of PokemonList objects and fix typo (pokemonlist -> pokemonList)

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [
   {name: "Pikachu" , height: 0.4, types: "electric", abilities: ["Static", "Lightningrod"]},
   {name: "Poliwag" , height: 0.6, types: "water", abilities: ["Damp","Water-absorb","Swift-swim"]},
+  {name: "Sharpedo" , height: 1.8, types: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
   {name: "Abra", height: 0.9, types: "psychic", abilities: ["Synchronize, Inner-focus, Magic-guard"]},
   {name: "Magnemite", height: 0.3, types: ["electric, steel"], abilities: ["Sturdy", "Magnet-pull", "Analytic"] },
-  {name: "Sharpedo" , height: 1.8, types: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
 ]
 
 let text= "";
 for (let i = 0; pokemonList[i];  i++) {
-if (pokemonlist[i].height > 1) {
+if (pokemonList[i].height > 1) {
   document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" + "<br>");
 } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br> ");
   }
 }
