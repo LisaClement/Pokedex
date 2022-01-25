c06590071471fe8f5b9eed0015c5ed81f86b3670
Lisa Clement <lisa.clement.a@gmail.com>
2022-01-25 15:49:15 +0100

    Wrap array in IIFE + add two new functions (getAll & add)

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
 
-(function (pokemon) {
-  let pokemonList = [
+let pokemonRepository= (function () {
+
+let pokemonList = [
   {name: "Pikachu" , height: 0.4, types: "electric", abilities: ["Static", "Lightningrod"]},
   {name: "Poliwag" , height: 0.6, types: "water", abilities: ["Damp","Water-absorb","Swift-swim"]},
   {name: "Sharpedo" , height: 1.8, types: ["dark", "water"], abilities:["Speed-boost", "Rough-skin"] },
   {name: "Abra", height: 0.9, types: "psychic", abilities: ["Synchronize, Inner-focus, Magic-guard"]},
   {name: "Magnemite", height: 0.3, types: ["electric, steel"], abilities: ["Sturdy", "Magnet-pull", "Analytic"] },
-];
+]
 
-  if (pokemon.height > 1) {
-   document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!" + "<br>");}
+function getAll() {
+  return pokemonList;
+}
 
-  else {
-   document.write(pokemon.name + " (height: " + pokemon.height + ") <br> ");
-   }
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
 
 })();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height > 1) {
+    document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow, that's big!" + "<br>");
+  } else {
+      document.write(pokemon.name + " (height: " + pokemon.height + ") <br> ");
+    }
+  });
